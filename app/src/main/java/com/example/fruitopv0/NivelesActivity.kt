7cee92afd789ca7fdfb7458e5e49d4020dc73ae4
Kevin-Mora <kevin.mora18@tectijuana.edu.mx>
2022-05-24 20:08:53 -0700

    Initial commit

---
+++ app/src/main/java/com/example/fruitopv0/NivelesActivity.kt
@@ -0,0 +1,26 @@
+package com.example.fruitopv0
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class NivelesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.niveles__activity)
+
+        // Boton para ir a a los niveles
+        // Creamos una nueva variable tipo Button y contiene el metodo findViewById(parametros
+        // en este caso R.id.idDelBoton)
+        val btn3: Button = findViewById(R.id.btn_puntuaciones)
+        // a traves de nuestro boton, llamamos a nuestra funcion escucha(lo que este dentro de
+        // nombreboton.setOnCLickListener{ }) en este caso a la variable intent y nuestro metodo
+        // intent que como parametros es this de esta pantalla a, nombre de la otra pantalla
+        // por ultimo con startActivity(intent) activamos nuestro intent
+        btn3.setOnClickListener {
+            val intent: Intent = Intent(this, PuntuacionesActivity:: class.java)
+            startActivity(intent )
+        }
+    }
+}
